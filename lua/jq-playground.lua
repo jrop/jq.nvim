4bfe0b73d80a87c6737f14faec657de950dccc62
Yochem van Rosmalen <git@yochem.nl>
2024-06-27 14:44:25 +0200

    Add stylua: use double quotes

--- lua/jq-playground.lua
+++ lua/jq-playground.lua
@@ -3,25 +3,25 @@ local M = {}
 local function user_preferred_indent(json_bufnr)
   local prefer_tabs = not vim.bo[json_bufnr].expandtab
   if prefer_tabs then
-    return '--tab'
+    return "--tab"
   else
     local indent_width = vim.bo[json_bufnr].softtabstop
-    return '--indent' .. indent_width
+    return "--indent" .. indent_width
   end
 end
 
 local function run_query(input_bufnr, query_bufnr, output_bufnr)
   local filter_lines = vim.api.nvim_buf_get_lines(query_bufnr, 0, -1, false)
-  local filter = table.concat(filter_lines, '\n')
+  local filter = table.concat(filter_lines, "\n")
 
-  local cmd = { 'jq', filter, user_preferred_indent(output_bufnr) }
+  local cmd = { "jq", filter, user_preferred_indent(output_bufnr) }
   local stdin = nil
 
   local modified = vim.bo[input_bufnr].modified
   local fname = vim.api.nvim_buf_get_name(input_bufnr)
 
   -- TODO: check if file actually exists?
-  if (not modified) and fname ~= '' then
+  if (not modified) and fname ~= "" then
     -- the following should be faster as it lets jq read the file contents
     table.insert(cmd, fname)
   else
@@ -31,16 +31,15 @@ local function run_query(input_bufnr, query_bufnr, output_bufnr)
   local ok, process = pcall(vim.system, cmd, { stdin = stdin })
 
   if not ok then
-    error('jq is not installed or not on your $PATH')
+    error("jq is not installed or not on your $PATH")
   end
 
   local result = process:wait()
   local output = result.code == 0 and result.stdout or result.stderr
 
-  local lines = vim.split(output, '\n', { plain = true })
+  local lines = vim.split(output, "\n", { plain = true })
   vim.api.nvim_buf_set_lines(output_bufnr, 0, -1, false, lines)
 end
-
 
 local function create_split(bufnr, winopts)
   local width = winopts.width
@@ -69,14 +68,14 @@ local function create_query_buffer(winopts)
 
   local winid = create_split(bufnr, winopts)
 
-  vim.bo[bufnr].filetype = 'jq'
-  vim.api.nvim_buf_set_name(bufnr, 'jq query editor')
+  vim.bo[bufnr].filetype = "jq"
+  vim.api.nvim_buf_set_name(bufnr, "jq query editor")
   vim.cmd.startinsert()
 
   vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {
-    '# JQ filter: press <CR> in normal mode to execute.',
-    '',
-    '',
+    "# JQ filter: press <CR> in normal mode to execute.",
+    "",
+    "",
   })
   vim.api.nvim_win_set_cursor(winid, { 3, 0 })
 
@@ -89,8 +88,8 @@ local function create_output_buffer(winopts)
 
   create_split(bufnr, winopts)
 
-  vim.bo[bufnr].filetype = 'json'
-  vim.api.nvim_buf_set_name(bufnr, 'jq output')
+  vim.bo[bufnr].filetype = "json"
+  vim.api.nvim_buf_set_name(bufnr, "jq output")
 
   return bufnr
 end
@@ -101,38 +100,39 @@ local function start_jq_buffers(opts)
   local output_json_bufnr = create_output_buffer(opts.output_window)
   local query_bufnr = create_query_buffer(opts.query_window)
 
-  vim.keymap.set('n', '<CR>', function()
+  vim.keymap.set("n", "<CR>", function()
     run_query(input_json_bufnr, query_bufnr, output_json_bufnr)
   end, {
     buffer = query_bufnr,
     silent = true,
-    desc = 'Run current jq query'
+    desc = "Run current jq query",
   })
 end
 
 function M.setup(opts)
   local defaults = {
     output_window = {
-      split_direction = 'right',
+      split_direction = "right",
       width = nil,
       height = nil,
     },
     query_window = {
-      split_direction = 'below',
+      split_direction = "below",
       width = nil,
       height = 0.3,
-    }
+    },
   }
 
   -- overwrite default options
   local options = vim.tbl_extend("force", defaults, opts)
 
-  vim.api.nvim_create_user_command('JqPlayground', function(params)
-    options['filename'] = params.fargs[1]
+  vim.api.nvim_create_user_command("JqPlayground", function(params)
+    -- also possible to use jq-playground without a source buffer
+    options["filename"] = params.fargs[1]
     start_jq_buffers(options)
   end, {
-    desc = 'Start jq query editor and live preview',
-    nargs = '?',
+    desc = "Start jq query editor and live preview",
+    nargs = "?",
   })
 end
 
